d01821588471a15ca622f1f6177a0464ad33276a
knakalunda <89476727+knakalunda@users.noreply.github.com>
2023-02-16 01:35:29 -0500

    Fixed activity

--- app/src/main/java/edu/temple/activity4/MainActivity.kt
+++ app/src/main/java/edu/temple/activity4/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -21,35 +22,38 @@ class MainActivity : AppCompatActivity() {
 
         // Trying to create array of integers that are multiples of 5
         // Verify correctness by examining array values.
-        val textSizes = Array(20){(it + 1) * 5}
+        val textSizes = Array(20) { (it + 1) * 5 }
 
-        for(i in 0 <= until < textSizes.size)
+        for (i in 0 until textSizes.size) {
             Log.d("Array values", textSizes[i].toString())
+        }
 
-        textSizeSelector.adapter = textSizeAdapter(textSizes)
-        textSizeSelector.layoutManager = LinearLayoutManager(context:this)
-    }
-}
-
-
-/* Convert to RecyclerView.Adapter */
-class TextSizeAdapter(_textSizes: Array<Int>) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
-    val textSizes = _textSizes
-    class TextSizeViewHolder(view: TextView) : RecyclerView.ViewHolder(view){
-        val textview = view
-    }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextSizeViewHolder {
-        return TextSizeViewHolder(TextView(parent.context).apply{setPadding(5,20,0,20)})
-    }
-
-    override fun getItemCount(): Int {
-        return textSizes.size
-    }
-
-    override fun onBindViewHolder(holder: TextSIzeViewHolder, position: Int) {
-        holder.textView.text = textSizes[position].toSting()
-        holder.textView.textSize = textSizes[position].toFloat
+        textSizeSelector.adapter = TextSizeAdapter(textSizes)
+        textSizeSelector.layoutManager = LinearLayoutManager(this)
     }
 
 
+    /* Convert to RecyclerView.Adapter */
+    class TextSizeAdapter(_textSizes: Array<Int>) :
+        RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
+        private val textSizes = _textSizes
+
+        class TextSizeViewHolder(view: TextView) : RecyclerView.ViewHolder(view) {
+            val textview = view
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextSizeViewHolder {
+            return TextSizeViewHolder(TextView(parent.context).apply { setPadding(5, 20, 0, 20) })
+        }
+
+        override fun getItemCount(): Int {
+            return textSizes.size
+        }
+
+        override fun onBindViewHolder(holder: TextSizeViewHolder, position: Int) {
+            holder.textview.text = textSizes[position].toString()
+            holder.textview.textSize = textSizes[position].toFloat()
+        }
+
+    }
 }
